f461e951609b3d7a221ef5d28e51f26a69901002
Viktor Bordiuzha <victor30608@yandex.ru>
2022-01-16 16:40:21 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,17 +1,3 @@
-# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-Run inference on images, videos, directories, streams, etc.
-
-Usage:
-    $ python path/to/detect.py --weights yolov5s.pt --source 0  # webcam
-                                                             img.jpg  # image
-                                                             vid.mp4  # video
-                                                             path/  # directory
-                                                             path/*.jpg  # glob
-                                                             'https://youtu.be/Zgi9g1ksQHc'  # YouTube
-                                                             'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
-"""
-
 import argparse
 import os
 import sys
@@ -20,7 +6,6 @@ from pathlib import Path
 import cv2
 import torch
 from tqdm import tqdm
-import torch.backends.cudnn as cudnn
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
